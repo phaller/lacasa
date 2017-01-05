0c40fd16abc35bf1051f738fdaa66d399986a8ca
Philipp Haller <hallerp@gmail.com>
2017-01-05 19:05:18 +0100

    Move SafeActor and SafeActorRef to separate file

--- akka-playground/src/main/scala/lacasa/akka/TestAkka.scala
+++ akka-playground/src/main/scala/lacasa/akka/TestAkka.scala
@@ -9,27 +9,6 @@ import lacasa.{Box, CanAccess, Safe, Packed, NoReturnControl}
 import Box.mkBox
 
 
-trait SafeActor[T] extends Actor {
-
-  // TODO: compiles even when commenting out the following implicit
-  implicit val loggingIsSafe = new Safe[LoggingAdapter] {}
-
-  def receive(msg: Box[T])(implicit acc: CanAccess { type C = msg.C }): Unit
-
-  def receive = {
-    case packed: Packed[T] =>
-      try {
-        receive(packed.box)(packed.access)
-      } catch {
-        case nrc: NoReturnControl => /* do nothing */
-          Box.uncheckedCatchControl
-      }
-
-    case _ => // internal error
-  }
-
-}
-
 case class Message(var s: String) {
   def this() = this("")
 }
@@ -45,19 +24,6 @@ class MyActor extends SafeActor[Message] {
       }
     })
   }
-}
-
-class SafeActorRef[T](private val ref: ActorRef) {
-  def ! (msg: Box[T])(implicit acc: CanAccess { type C = msg.C }): Nothing = {
-    // have to create a `Packed[T]`
-    ref ! msg.pack()  // `pack()` accessible within package `lacasa`
-    throw new NoReturnControl
-  }
-}
-
-object SafeActorRef {
-  def apply[T](ref: ActorRef): SafeActorRef[T] =
-    new SafeActorRef[T](ref)
 }
 
 
